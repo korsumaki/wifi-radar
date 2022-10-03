992063a5045c8282d2c51770931ec44f4a6c86d3
Markku Korsumäki <markku.korsumaki@gmail.com>
2022-10-03 20:54:15 +0300

    Add Scan button to add random WifiAp to scanList

--- app/src/main/java/com/example/wifiradar/MainActivity.kt
+++ app/src/main/java/com/example/wifiradar/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.wifiradar.ui.theme.WiFiRadarTheme
@@ -22,17 +25,28 @@ import com.example.wifiradar.ui.theme.WiFiRadarTheme
 *  - distance calculation
 *  - map chart element on UI
 *  - more advanced methods for getting distance
+*       https://developer.android.com/guide/topics/connectivity/wifi-rtt
+*       https://github.com/Plinzen/android-rttmanager-sample
 *  - BT scanning
 *
 * TODO application
-*  - App layout
+*  - viewModel
+*  - separate
+*       - MainActivity
+*       - Composables
+*       - Data classes
+*  - App layout (Scaffold)
 *  - icon
 *  - enable crashlytics
+*  - enable leakcanary
 *  - publish to play store
+*  - compose testing?
+*       https://developer.android.com/jetpack/compose/testing
 *
 * TODO project/github
 *  - project description
 *  - readme
+*  - package name -> korsumaki
 *
 */
 
@@ -46,19 +60,14 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ScanListScreen(
-                        listOf(
-                            WifiAp(name="eka", strength = 14),
-                            WifiAp(name="toka", strength = 65)
-                        )
-                    )
+                    WifiRadarScreen()
                 }
             }
         }
     }
 }
 
-class WifiAp(val name: String, var strength: Int = 100)
+data class WifiAp(val name: String, var strength: Int = 100)
 
 @Composable
 fun ScanListItem(ap: WifiAp) {
@@ -69,7 +78,7 @@ fun ScanListItem(ap: WifiAp) {
 }
 
 @Composable
-fun ScanListScreen(wifiApList: List<WifiAp>) {
+fun ScanListScreen(wifiApList: List<WifiAp>, onScanButtonPress: () -> Unit ) { /*MutableList*/
     Column {
         Text(
             text = "WiFi Radar",
@@ -80,9 +89,29 @@ fun ScanListScreen(wifiApList: List<WifiAp>) {
                 ScanListItem(ap = wifiAp)
             }
         }
+        Button(onClick = onScanButtonPress) {
+            Text(text = "Scan")
+        }
     }
 }
 
+fun getRandomWifiAp(): WifiAp {
+    val nameNumberRange = 1..10
+    val strengthRange = 30..100
+    val ap = WifiAp(name = "AP-${nameNumberRange.random()}", strength = strengthRange.random() )
+    println("getRandomWifiAp(): $ap")
+    return ap
+}
+
+@Composable
+fun WifiRadarScreen() {
+    val scanList = remember { mutableStateListOf<WifiAp>() }
+
+    ScanListScreen(
+        scanList,
+        onScanButtonPress = { scanList.add(getRandomWifiAp()) }
+    )
+}
 
 @Preview(showBackground = true)
 @Composable
@@ -97,11 +126,20 @@ fun ScanListItemPreview() {
 fun ScanListPagePreview() {
     WiFiRadarTheme {
         ScanListScreen(
-            listOf(
+            listOf( //mutableListOf
                 WifiAp(name="eka"),
                 WifiAp(name="toka", strength = 65),
                 WifiAp(name="kolmas", strength = 97)
-            )
+            ),
+            onScanButtonPress = { println("scan pressed") }
         )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WifiRadarScreenPreview() {
+    WiFiRadarTheme {
+        WifiRadarScreen()
     }
 }
