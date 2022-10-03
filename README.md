497f6798e6dfec31cbef5089b09b0da787271e0e
Markku Korsumäki <markku.korsumaki@gmail.com>
2022-10-03 21:45:21 +0300

    Move TODO list from MainActivity to README.md

--- README.md
+++ README.md
@@ -4,4 +4,30 @@ WiFi Radar is (experimental) application to scan WiFi access points and create m
 
 ## TODO list
 
-- some things to implement
+Here is list of ideas what to do
+
+- Functionality
+  - WiFi scanning
+  - distance calculation
+  - map chart element on UI
+  - more advanced methods for getting distance
+    - https://developer.android.com/guide/topics/connectivity/wifi-rtt
+    - https://github.com/Plinzen/android-rttmanager-sample
+  - BT scanning
+
+- Application
+  - remember list during orientation change
+  - scroll scanList
+  - viewModel
+  - App layout (Scaffold)
+  - icon
+  - enable crashlytics
+  - enable leakcanary
+  - publish to play store
+  - compose testing?
+    - https://developer.android.com/jetpack/compose/testing
+
+- TODO project/github
+  - project description
+  - readme
+  - package name -> korsumaki
--- app/src/main/java/com/example/wifiradar/MainActivity.kt
+++ app/src/main/java/com/example/wifiradar/MainActivity.kt
@@ -9,39 +9,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.wifiradar.ui.theme.WiFiRadarTheme
 
-/*
-* TODO functionality
-*  + ScanList on UI
-*  - WiFi scanning
-*  - distance calculation
-*  - map chart element on UI
-*  - more advanced methods for getting distance
-*       https://developer.android.com/guide/topics/connectivity/wifi-rtt
-*       https://github.com/Plinzen/android-rttmanager-sample
-*  - BT scanning
-*
-* TODO application
-*  - remember list during orientation change
-*  - scroll scanList
-*  - viewModel
-*  + separate
-*       + MainActivity
-*       + Composables
-*       + Data classes
-*  - App layout (Scaffold)
-*  - icon
-*  - enable crashlytics
-*  - enable leakcanary
-*  - publish to play store
-*  - compose testing?
-*       https://developer.android.com/jetpack/compose/testing
-*
-* TODO project/github
-*  - project description
-*  - readme
-*  - package name -> korsumaki
-*
-*/
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
