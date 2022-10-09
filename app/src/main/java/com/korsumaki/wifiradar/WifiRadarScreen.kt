aadf429998251bfb61d02186a16b5c5114df0017
Markku Korsumäki <markku.korsumaki@gmail.com>
2022-10-09 20:19:25 +0300

    Add Compose Canvas to visualize ForceGraph

--- app/src/main/java/com/korsumaki/wifiradar/WifiRadarScreen.kt
+++ app/src/main/java/com/korsumaki/wifiradar/WifiRadarScreen.kt
@@ -12,6 +12,12 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.foundation.Canvas
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+//import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.korsumaki.wifiradar.ui.theme.WiFiRadarTheme
@@ -19,6 +25,7 @@ import com.korsumaki.wifiradar.ui.theme.WiFiRadarTheme
 
 @Composable
 fun WifiRadarScreen(activity: Activity) {
+    /*
     val scanList = remember { mutableStateListOf<WifiAp>() }
     val scanner = WiFiRadarScanner(activity = activity, scanList)
 
@@ -28,7 +35,76 @@ fun WifiRadarScreen(activity: Activity) {
             scanner.scan()
         }
     )
+    */
+
+    val forceGraph = remember { mutableStateOf<ForceGraph>(ForceGraph()) }
+
+    val centerNode = ForceNode("Center", Coordinate(600f,600f))
+
+    MapScreen(
+        forceGraph = forceGraph.value,
+        onAddNodeButtonPress = {
+            println("onAddNodeButtonPress")
+            val coordinateRange = 200..1000
+            val newNode = ForceNode(
+                id = "new-${forceGraph.value.nodeList.size}",
+                coordinate = Coordinate(coordinateRange.random().toFloat(), coordinateRange.random().toFloat()))
+
+            forceGraph.value.connectNodesWithRelation(centerNode, newNode, ForceRelation(100f))
+        },
+        onIterateButtonPress = {
+            println("onIterateButtonPress")
+            forceGraph.value.iterateRelations()
+        }
+    )
 }
+
+
+@Composable
+fun MapScreen(forceGraph: ForceGraph, onAddNodeButtonPress: () -> Unit, onIterateButtonPress: () -> Unit) {
+    Column {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "WiFi Map",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.padding(8.dp)
+            )
+            Button(
+                onClick = { onAddNodeButtonPress() },
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Text(text = "Add node")
+            }
+            Button(
+                onClick = { onIterateButtonPress() },
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Text(text = "Iterate")
+            }
+        }
+        Canvas(modifier = Modifier.fillMaxSize()) {
+
+            for (relation in forceGraph.relationList) {
+                if (relation.coordinateList.size == 2) {
+                    drawLine(
+                        color = Color.LightGray,
+                        start = Offset(relation.coordinateList[0].x, relation.coordinateList[0].y),
+                        end = Offset(relation.coordinateList[1].x, relation.coordinateList[1].y),
+                        strokeWidth = 5f
+                    )
+                }
+            }
+
+            for (node in forceGraph.nodeList) {
+                drawCircle(Color.Red, center = Offset(node.coordinate.x, node.coordinate.y), radius = 20f)
+            }
+        }
+    }
+}
+
+
 
 @Composable
 fun ScanListScreen(wifiApList: List<WifiAp>, onScanButtonPress: () -> Unit ) {
@@ -72,6 +148,34 @@ fun WifiRadarScreenPreview() {
         WifiRadarScreen(null)
     }
 }*/
+
+
+@Preview(showBackground = true)
+@Composable
+fun MapScreenPreview() {
+    val forceGraph = remember { mutableStateOf<ForceGraph>(ForceGraph()) }
+
+    WiFiRadarTheme {
+        val centerNode = ForceNode("Center", Coordinate(600f,600f))
+
+        MapScreen(
+            forceGraph = forceGraph.value,
+            onAddNodeButtonPress = {
+                println("onAddNodeButtonPress")
+                val coordinateRange = 200..1000
+                val newNode = ForceNode(
+                    id = "new-${forceGraph.value.nodeList.size}",
+                    coordinate = Coordinate(coordinateRange.random().toFloat(), coordinateRange.random().toFloat()))
+
+                forceGraph.value.connectNodesWithRelation(centerNode, newNode, ForceRelation(100f))
+            },
+            onIterateButtonPress = {
+                println("onIterateButtonPress")
+                forceGraph.value.iterateRelations()
+            }
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
