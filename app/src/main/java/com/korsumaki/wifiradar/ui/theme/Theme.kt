5f5cc70765af1d8a60adbcb243c80894e6a9ebc0
Markku Korsumäki <markku.korsumaki@gmail.com>
2025-03-01 15:29:04 +0200

    Bump targetSdk and compileSdk to 35
    remove deprecated statusBarColor

--- app/src/main/java/com/korsumaki/wifiradar/ui/theme/Theme.kt
+++ app/src/main/java/com/korsumaki/wifiradar/ui/theme/Theme.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -52,8 +51,9 @@ fun WifiRadarTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            // TODO Deprecated
+            //window.statusBarColor = colorScheme.primary.toArgb()
+        WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
 
